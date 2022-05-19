e2f1ec9817063d64589e7e78beb0858c5b5061f1
wjknnn <wjknn3123@gmail.com>
2022-05-19 15:03:58 +0900

    초기 커밋.

---
+++ BAEKJOON/Baekjoon.cpp
@@ -0,0 +1,52 @@
+//입출력과 사칙연산
+/*
+#include<stdio.h> // 2557  : Hello World
+
+int main() {
+	printf("Hello World!");
+	return 0;
+}
+
+#include<stdio.h> // 10718 : We love kriii
+
+int main() {
+	printf("강한친구 대한육군\n강한친구 대한육군");
+	return 0;
+}
+
+#include<stdio.h> // 10171 : 고양이
+
+int main() {
+	printf("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+	return 0;
+}
+
+#include<stdio.h> // 225083 : 새싹
+
+int main() {
+	printf("         ,r\'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r\'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |");
+	return 0;
+}
+
+#include<stdio.h> // 1000  : A+B
+
+int main() {
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d", A + B);
+	return 0;
+}
+
+#include<stdio.h> // 1001  : A-B
+
+int main() {
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d", A - B);
+	return 0;
+}*/
