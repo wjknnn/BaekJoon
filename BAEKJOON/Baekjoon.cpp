e60fcd92a2c3418a7a50b0f9e00586f19bab273b
wjknnn <wjknn3123@gmail.com>
2022-05-19 19:57:01 +0900

    입출력과 사칙연산 끝

--- BAEKJOON/Baekjoon.cpp
+++ BAEKJOON/Baekjoon.cpp
@@ -1,20 +1,20 @@
 //입출력과 사칙연산
 /*
-#include<stdio.h> // 2557  : Hello World
+#include<stdio.h> // 2557   : Hello World
 
 int main() {
 	printf("Hello World!");
 	return 0;
 }
 
-#include<stdio.h> // 10718 : We love kriii
+#include<stdio.h> // 10718  : We love kriii
 
 int main() {
 	printf("강한친구 대한육군\n강한친구 대한육군");
 	return 0;
 }
 
-#include<stdio.h> // 10171 : 고양이
+#include<stdio.h> // 10171  : 고양이
 
 int main() {
 	printf("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
@@ -33,7 +33,7 @@ int main() {
 	return 0;
 }
 
-#include<stdio.h> // 1000  : A+B
+#include<stdio.h> // 1000   : A+B
 
 int main() {
 	int A, B;
@@ -42,11 +42,80 @@ int main() {
 	return 0;
 }
 
-#include<stdio.h> // 1001  : A-B
+#include<stdio.h> // 1001   : A-B
 
 int main() {
 	int A, B;
 	scanf("%d %d", &A, &B);
 	printf("%d", A - B);
+	return 0;
+}
+
+#include<stdio.h> // 10998  : A*B
+
+int main() {
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d", A * B);
+	return 0;
+}
+
+#include<stdio.h> // 1008   : A/B
+
+int main() {
+	double A, B;
+	scanf("%lf %lf", &A, &B);
+	printf("%.9lf", A / B);
+	return 0;
+}
+
+#include<stdio.h> // 10869  : 사칙연산
+
+int main() {
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d\n%d\n%d\n%d\n%d\n", A + B, A - B, A * B, A / B, A % B);
+	return 0;
+}
+
+#include<stdio.h> // 10926  : ??!
+
+int main() {
+	char Id[51] = { 0 };
+	scanf("%s", &Id);
+	printf("%s??!", Id);
+	return 0;
+}
+
+#include<stdio.h> // 18108  : 1998년생인 내가 태국에서는 2541년생?!
+
+int main() {
+	int y;
+	scanf("%d", &y);
+	printf("%d", y - 543);
+	return 0;
+}
+
+#include<stdio.h> // 10430  : 나머지
+
+int main() {
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+	printf("%d\n", (A + B) % C);
+	printf("%d\n", ((A % C) + (B % C)) % C);
+	printf("%d\n", (A * B) % C);
+	printf("%d", ((A % C) * (B % C)) % C);
+	return 0;
+}
+
+#include<stdio.h> // 2588   : 곱셈
+
+int main() {
+	int num1, num2, il, sib, back;
+	scanf("%d %d", &num1, &num2);
+	il = num1 * (num2 % 10);
+	sib = num1 * (num2 % 100 / 10);
+	back = num1 * (num2 % 1000 / 100);
+	printf("%d\n%d\n%d\n%d", il, sib, back, num1 * num2);
 	return 0;
 }*/
