c32bca5140ee23facbd62eb27704e5a88dd9b4a8
SeungWoo <wjknn3123@gmail.com>
2022-09-13 23:24:21 +0900

    Commit

--- BAEKJOON/Baekjoon.cpp
+++ BAEKJOON/Baekjoon.cpp
@@ -145,7 +145,58 @@ int main() {
 }*/
 
 //Silver
-/**/
+/*
+#include<stdio.h> // 17478  : 재귀함수가 뭔가요?
+
+int i = 0;
+int underhamsu(int i) {
+	for (int n = 0; n < i; n++) {
+		printf("____");
+	}
+	return 1;
+}
+
+int answer(int i) {
+	for (int n = i - 1; n >= 0; n--) {
+		printf("\n");
+		underhamsu(n);
+		printf("라고 답변하였지.");
+	}
+	return 1;
+}
+
+int jaeguihamsu(int n) {
+	if (i < n) {
+		underhamsu(i);
+		printf("\"재귀함수가 뭔가요?\"\n");
+		underhamsu(i);
+		printf("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+		underhamsu(i);
+		printf("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+		underhamsu(i);
+		printf("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+		i++;
+		return jaeguihamsu(n);
+	}
+	else if (i == n) {
+		underhamsu(i);
+		printf("\"재귀함수가 뭔가요?\"\n");
+		underhamsu(i);
+		printf("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+		underhamsu(i);
+		printf("라고 답변하였지.");
+		answer(n);
+	}
+	return 0;
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+	jaeguihamsu(n);
+	return 0;
+}*/
 
 //Gold
 /*
